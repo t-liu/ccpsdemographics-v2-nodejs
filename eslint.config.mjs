4fc506b3be6f2e6d7920c7d1d0cbaa70f9e4ccbd
t-liu <thomas.s.liu@gmail.com>
2026-05-24 23:10:38 -0400

    update eslint config to recognize node.js + jest tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,30 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { ignores: ["node_modules/**", ".serverless/**", "coverage/**"] },
+  js.configs.recommended,
+  {
+    files: ["src/**/*.js"],
+    languageOptions: {
+      globals: globals.node,
+      sourceType: "commonjs",
+    },
+    rules: {
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
+  {
+    files: ["tests/**/*.js"],
+    languageOptions: {
+      globals: { ...globals.node, ...globals.jest },
+      sourceType: "commonjs",
+    },
+  },
+  {
+    files: ["eslint.config.mjs"],
+    languageOptions: {
+      globals: globals.node,
+      sourceType: "module",
+    },
+  },
 ]);
